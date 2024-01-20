685ea4afcb4d6ec7c1745c0f9c954b80b2d5e071
Gan Ze Yu <52880557+zeyugan@users.noreply.github.com>
2024-01-20 17:43:48 +0800

    larger button

--- frontend/src/components/WalletCard.tsx
+++ frontend/src/components/WalletCard.tsx
@@ -3,25 +3,37 @@ import { Button, ButtonGroup, Input } from "@nextui-org/react";
 
 const WalletCard: React.FC = () => {
   return (
-    <div className="flex flex-col items-center  rounded-lg p-8">
+    <div className="flex flex-col items-center rounded-lg p-8">
       <h2 className="text-white text-lg font-semibold mb-6">VIEW RESULTS</h2>
       <div className="w-full grid grid-cols-3 gap-4">
         {/* Div 1 */}
         <div className="flex flex-col items-center justify-center space-y-2">
-        <Button radius="md" className="bg-lime-500 text-white shadow-lg">Button 1</Button>
-          <Input fullWidth color="success" placeholder="Enter text 1" />
+          <Button
+            radius="md"
+            className="bg-lime-500 text-white shadow-lg text-xl px-6 py-3"
+          >
+            Button 1
+          </Button>
         </div>
 
         {/* Div 2 */}
         <div className="flex flex-col items-center justify-center space-y-2">
-        <Button radius="md" className="bg-lime-500 text-white shadow-lg">Button 1</Button>
-          <Input fullWidth color="success" placeholder="Enter text 2" />
+          <Button
+            radius="md"
+            className="bg-lime-500 text-white shadow-lg text-xl px-6 py-3"
+          >
+            Button 2
+          </Button>
         </div>
 
         {/* Div 3 */}
         <div className="flex flex-col items-center justify-center space-y-2">
-        <Button radius="md" className="bg-lime-500 text-white shadow-lg">Button 1</Button>
-          <Input fullWidth color="success" placeholder="Enter text 3" />
+          <Button
+            radius="md"
+            className="bg-lime-500 text-white shadow-lg text-xl px-6 py-3"
+          >
+            Button 3
+          </Button>
         </div>
       </div>
     </div>
